794d764bc738735af7d2eaff17b441990c4b738b
Bába Kristóf <kristof@myhost.local>
2023-05-10 14:51:50 +0300

    giftcard code model hotfix

--- main_website/models/giftcard_code.py
+++ main_website/models/giftcard_code.py
@@ -7,6 +7,10 @@ class GiftcardCode(models.Model):
     code = models.CharField(
         max_length=14, default="0000-0000-0000", blank=False, null=False
     )
+    for_sale = models.BooleanField(
+        default=True,
+        help_text="If this is not active, it means it was sold!",
+    )
 
     def __str__(self):
-        return str(self.giftcard)
+        return f"NAME: {self.giftcard} | FOR SALE: {self.for_sale}"
