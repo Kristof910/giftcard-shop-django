4c7788e5a422393375684b9884281d0d5a896899
Bába Kristóf <kristof@myhost.local>
2023-06-02 19:30:39 +0300

    stock limits purchases

--- main_website/views/giftcard_detail_view.py
+++ main_website/views/giftcard_detail_view.py
@@ -5,7 +5,15 @@ from ..models.giftcard_code import GiftcardCode
 
 # using method based view for easier access of type.description field
 def giftcard_detail_view(request, pk):
+    cart_list = request.session.get("shopping_cart")
+    cart_stock = cart_list.count(pk)
+
     giftcard = Giftcard.objects.get(pk=pk)
-    stock = GiftcardCode.objects.filter(giftcard=giftcard, for_sale=True).count()
-    context = {"giftcard": giftcard, "pk": pk, "stock": stock}
+    total_stock = GiftcardCode.objects.filter(giftcard=giftcard, for_sale=True).count()
+    context = {
+        "giftcard": giftcard,
+        "pk": pk,
+        "total_stock": total_stock,
+        "cart_stock": cart_stock,
+    }
     return render(request, "giftcard_detail.html", context)
