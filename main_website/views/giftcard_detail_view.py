b7c16df24ef9b392241616d05e991f4eb89b1639
Bába Kristóf <kristof@myhost.local>
2023-06-07 17:08:38 +0300

    hot fix

--- main_website/views/giftcard_detail_view.py
+++ main_website/views/giftcard_detail_view.py
@@ -5,8 +5,12 @@ from ..models.giftcard_code import GiftcardCode
 
 # using method based view for easier access of type.description field
 def giftcard_detail_view(request, pk):
-    cart_list = request.session.get("shopping_cart")
-    cart_stock = cart_list.count(pk)
+    # checking cart count for proper error message
+    if "shopping_cart" in request.session and request.session.get("shopping_cart"):
+        cart_list = request.session.get("shopping_cart")
+        cart_stock = cart_list.count(pk)
+    else:
+        cart_stock = 0
 
     giftcard = Giftcard.objects.get(pk=pk)
     total_stock = GiftcardCode.objects.filter(giftcard=giftcard, for_sale=True).count()
